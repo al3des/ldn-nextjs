22e4a3a0ef0e8cabac6e97d3e0896bd9240aa12c
al3des <alejandrodesalvo@gmail.com>
2022-06-27 08:37:37 +0200

    test

--- pages/api/invites/updateList.js
+++ pages/api/invites/updateList.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   try {
     // const isValid = verifyWebhookSignature({ body, signature, secret });
     if (isValid) {
-        await res.revalidate('/invites/list');
+        await res.unstable_revalidate('/invites/list');
       return res.status(200).json({ message: "success, revalidated" });
     }
   } catch (error) {
