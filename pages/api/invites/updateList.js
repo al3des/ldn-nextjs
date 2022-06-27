167091b1e9913cc14d5af80202a1603e7d2e1a3d
al3des <alejandrodesalvo@gmail.com>
2022-06-27 10:56:36 +0200

    api test

--- pages/api/invites/updateList.js
+++ pages/api/invites/updateList.js
@@ -1,15 +1,16 @@
-const { verifyWebhookSignature } = require ("@graphcms/utils");
+const { verifyWebhookSignature } = require("@graphcms/utils");
 
 export default async function handler(req, res) {
   const secret = process.env.GRAPHCMS_WEBHOOK_SECRET;
 
   const body = req.body; // Typically req.body
   const signature = req.headers["gcms-signature"]; // Typically req.headers['gcms-signature']
+  console.log(req.headers["gcms-signature"]);
   // const isValid = true
   try {
     const isValid = verifyWebhookSignature({ body, signature, secret });
     if (isValid) {
-        await res.unstable_revalidate('/invites/list');
+      await res.unstable_revalidate("/invites/list");
       return res.status(200).json({ message: "success, revalidated" });
     }
   } catch (error) {
