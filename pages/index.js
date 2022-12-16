a5b218daf9454ee1641dc7a98d5f897803d3fc14
al3des <alejandrodesalvo@gmail.com>
2022-12-16 01:42:13 +0100

    members as home

--- pages/index.js
+++ pages/index.js
@@ -1,143 +1,190 @@
-import logo from "../src/assets/logo.svg";
-import styles from "../src/styles/home.module.css";
-
-import InstagramIcon from "@mui/icons-material/Instagram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import AnimatedBackgroundElements from "../src/components/widgets/animated-background-elements";
+import React from "react";
 import Image from "next/image";
-import { Typography } from "@mui/material";
-import UpcomingEvents from "../src/components/widgets/upcoming-events";
-import { getAllEvents } from "../src/lib/graphcms";
 
-function App({ upcomingEvents, pastEvents }) {
+import { List, ListItem, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { getAllMembers, getPageBySlug } from "../src/lib/graphcms";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+
+import logo from "../src/assets/logo-fuccia.svg";
+import styles from "../src/styles/members.module.css";
+import YoutubeEmbed from "../src/components/widgets/youtube-embed";
+
+export default function Members(props) {
+  const [loadVideo, setLoadVideo] = React.useState(false);
+  const videos = ["1uxt6RqGg-k", "aLe_wzVaul8"];
   return (
     <>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-          // backgroundColor: "#ff5d8f",
+          bgcolor: "background.paper",
+          minHeight: "100vh",
         }}
       >
-        <Box
-          className={styles.logo}
-          sx={{ position: "relative", width: "100%" }}
-        >
-          <Image
-            src={logo}
-            alt="La Doble Nelson"
-            objectFit="contain"
-            layout="fill"
-          />
-        </Box>
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            "& .MuiLink-root": { color: "white" },
+            alignItems: "center",
+            gap: 5,
+            px: 5,
+            justifyContent: "center",
           }}
         >
-          <Link
-            href="https://www.instagram.com/ladoblenelsonberlin/"
-            target="_blank"
-            rel="noopener noreferrer"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-              textDecoration: "none",
-            }}
-          >
-            <InstagramIcon />
-            Instagram
-          </Link>
-          <Link
-            href="https://www.youtube.com/channel/UCUZXdBepW0y5RFp9VUgkaxA"
-            target="_blank"
-            rel="noopener noreferrer"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-              textDecoration: "none",
-            }}
-          >
-            <YouTubeIcon />
-            YouTube
-          </Link>
+          <Image src={logo} width={300} height={300} />
+          {/* <Typography variant="h2">is:</Typography> */}
         </Box>
-      </Box>
-      {/* end of main */}
-      {/* ANIMATED BACKGROUND */}
-      <Box className={styles.background} sx={{ zIndex: -9999 }}>
-        <AnimatedBackgroundElements itemsCount={20} />
-      </Box>
-      {/* EVENTS */}
-      <Box
-        id="events"
-        sx={{
-          minHeight: "100vh",
-          mx: "auto",
-          p: 6,
-          backgroundColor: "background.paper",
-          align: "center",
-        }}
-      >
-        {upcomingEvents.length > 0 && (
-          <>
-            <Typography
-              className={styles.neonText}
-              color="primary"
-              variant="h4"
-              component="h2"
-              align="center"
-              sx={{ maxWidth: "sm", mx: "auto", mb: 10 }}
-            >
-              Upcoming events
+
+        <Box
+          sx={{
+            bgcolor: "secondary.main",
+            display: "flex",
+            flexDirection: ["column-reverse", "column-reverse", "row"],
+          }}
+        >
+          <Box
+            sx={{
+              p: 5,
+              maxWidth: ["100%", "100%", "80%"],
+              mx: "auto",
+            }}
+          >
+            <Typography variant="h2" className={styles.underlinedTitle}>
+              About
             </Typography>
-            <UpcomingEvents items={upcomingEvents} />
-          </>
-        )}
-        {pastEvents.length > 0 && (
-          <>
-            <Typography
-              className={styles.neonText}
-              color="primary"
-              variant="h4"
-              component="h2"
-              align="center"
-              sx={{ maxWidth: "sm", mx: "auto", mb: 10 }}
+            <Typography variant="h5">{props.pages.text}</Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              minWidth: "50%",
+              minHeight: "300px",
+              flexGrow: 1,
+            }}
+          >
+            <Image src="/stage.jpg" layout="fill" objectFit="cover" />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            py: 0,
+            bgcolor: "primary.main",
+          }}
+        >
+          {(!loadVideo && (
+            <Box
+              sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                gap: [0, 3, 5],
+                py: 5,
+              }}
             >
-              Past events
-            </Typography>
-            <UpcomingEvents items={pastEvents} />
-          </>
-        )}
+              <>
+                <Typography variant="h1" component="h3">
+                  Meet
+                </Typography>
+                <Box sx={{ flexGrow: 0, fontSize: "10rem", display: "flex" }}>
+                  <PlayCircleOutlineIcon
+                    fontSize="inherit"
+                    onClick={() => setLoadVideo(true)}
+                    sx={{
+                      "&:hover": {
+                        cursor: "pointer",
+                      },
+                    }}
+                  />
+                </Box>
+                <Typography variant="h1" component="h3">
+                  Us
+                </Typography>
+              </>
+            </Box>
+          )) || (
+            <Box sx={{ p: 5 }}>
+              <YoutubeEmbed embedId={shuffleArray(videos)[0]} autoplay={true} />
+            </Box>
+          )}
+        </Box>
+        <Box
+          sx={{
+            p: 5,
+          }}
+        >
+          <Typography variant="h2" className={styles.underlinedTitle}>
+            We are
+          </Typography>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: [
+                "1fr",
+                "1fr",
+                "repeat(auto-fill, minmax(200px, 1fr))",
+              ],
+              gap: 2,
+              flexGrow: 1,
+            }}
+          >
+            {shuffleArray(props.members).map((member) => (
+              <Box key={member.id} sx={{ position: "relative" }}>
+                <Typography
+                  variant="h3"
+                  sx={{
+                    position: "absolute",
+                    bottom: 0,
+                    left: 0,
+                    zIndex: 99999,
+                    p: 2,
+                  }}
+                >
+                  {member.name}
+                </Typography>
+                <Box
+                  className={styles.roundedImage}
+                  sx={{ position: "relative", width: "100%", height: "400px" }}
+                >
+                  <Image
+                    src={member.profilePicture.url}
+                    layout="fill"
+                    objectFit="cover"
+                    className={styles.roundedImage}
+                  />
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        </Box>
       </Box>
     </>
   );
 }
-export default App;
 
-export async function getStaticProps() {
-  const events = await getAllEvents();
-  const upcomingEvents = events.filter(
-    (event) => new Date(event.date) > new Date()
-  );
-  const pastEvents = events.filter(
-    (event) => new Date(event.date) < new Date()
-  );
+export async function getStaticProps({ locales, locale }) {
+  const members = await getAllMembers();
+  const pages = await getPageBySlug(locale);
   return {
     props: {
-      upcomingEvents,
-      pastEvents,
+      members,
+      pages,
     },
-    revalidate: 10
+    revalidate: 30,
   };
+}
+
+function shuffleArray(array) {
+  let curId = array.length;
+  // There remain elements to shuffle
+  while (0 !== curId) {
+    // Pick a remaining element
+    let randId = Math.floor(Math.random() * curId);
+    curId -= 1;
+    // Swap it with the current element.
+    let tmp = array[curId];
+    array[curId] = array[randId];
+    array[randId] = tmp;
+  }
+  return array;
 }
