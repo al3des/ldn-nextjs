604af68504b5f4cc622d56c5ccc8b610d91962a2
devie <devie@Air-von-devie.fritz.box>
2023-04-12 19:58:02 +0200

    added soundcloud

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import { RiSoundcloudLine } from "react-icons/ri";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import Link from "../../Link";
 
@@ -10,38 +11,57 @@ export default function Footer() {
   return (
     <Box
       sx={(theme) => ({
-          bgcolor: 'background.default',
+        bgcolor: "background.default",
         color: theme.palette.aqua.main,
         p: 5,
       })}
     >
       <List>
         <ListItem>
-          <ListItemIcon sx={{color: 'inherit'}}>
+          <ListItemIcon sx={{ color: "inherit" }}>
             <InstagramIcon />
           </ListItemIcon>
           <ListItemText>
-            <Link href="https://instagram.com/ladoblenelsonberlin" target='_blank'>
+            <Link
+              href="https://instagram.com/ladoblenelsonberlin"
+              target="_blank"
+            >
               Instagram
             </Link>
           </ListItemText>
         </ListItem>
         <ListItem>
-          <ListItemIcon sx={{color: 'inherit'}}>
+          <ListItemIcon sx={{ color: "inherit" }}>
             <YouTubeIcon />
           </ListItemIcon>
           <ListItemText>
-            <Link href="https://www.youtube.com/channel/UCUZXdBepW0y5RFp9VUgkaxA" target='_blank'>
+            <Link
+              href="https://www.youtube.com/@ladoblenelsonberlin"
+              target="_blank"
+            >
               YouTube
             </Link>
           </ListItemText>
         </ListItem>
         <ListItem>
-          <ListItemIcon sx={{color: 'inherit'}}>
+          <ListItemIcon sx={{ color: "inherit", fontSize: 24 }}>
+            <RiSoundcloudLine />
+          </ListItemIcon>
+          <ListItemText>
+            <Link
+              href="https://soundcloud.com/ladoblenelsonberlin"
+              target="_blank"
+            >
+              SoundCloud
+            </Link>
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon sx={{ color: "inherit" }}>
             <AlternateEmailIcon />
           </ListItemIcon>
           <ListItemText>
-            <Link href="mailto:info@ladoblenelson.de" target='_blank'>
+            <Link href="mailto:info@ladoblenelson.de" target="_blank">
               info@ladoblenelson.de
             </Link>
           </ListItemText>
